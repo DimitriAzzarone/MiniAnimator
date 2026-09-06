a55724477b7ec91274f5b6371a3e6783859cab4a
Dimitri Azzarone <LA_TUA_EMAIL_GITHUB>
2026-09-06 20:25:08 +0200

    MiniAnimator 3.0 desktop-like WebView editor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.dimitriazzarone.minianimator"
         minSdk = 26
         targetSdk = 35
-        versionCode = 21
-        versionName = "2.1"
+        versionCode = 30
+        versionName = "3.0"
     }
 
     compileOptions {
